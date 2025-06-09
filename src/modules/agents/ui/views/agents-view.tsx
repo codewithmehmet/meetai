dcaf6ecf94090696b86c668a978acfe44000a970
codewithmehmet <codewithmehmet@gmail.com>
2025-06-09 11:51:50 +0200

    feat: add agents filters

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,23 +1,34 @@
 "use client";
-
 import { EmptyState } from "@/components/empty-state";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { columns } from "../components/columns";
+import { DataPagination } from "../components/data-pagination";
 import { DataTable } from "../components/data-table";
 
 export const AgentsView = () => {
+  const [filters, setFilters] = useAgentsFilters();
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
 
   return (
-    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data} columns={columns} />
-      {data.length === 0 && (
+    <div className="flex-1 fb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data.items} columns={columns} />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.items.length === 0 && (
         <EmptyState
-          title="Create your first agent"
+          title="Create your first Agent"
           description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."
         />
       )}
@@ -29,7 +40,7 @@ export const AgentsViewLoading = () => {
   return (
     <LoadingState
       title="Loading Agents"
-      description="This may take a few seconds"
+      description="This may take a fews econds"
     />
   );
 };
